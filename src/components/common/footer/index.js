ebf9c0019c78f7488b26ac0779d88651b703d0dc
srinivas <seenucnu473@gmail.com>
2024-01-26 11:31:14 +0530

    zomato

---
+++ src/components/common/footer/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="absolute-center max-width">Created 🔥 by Srinivas Poosa</div>
+  );
+};
+
+export default Footer;
